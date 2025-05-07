2a16b275f02e38f027ffd28412a96811cb1df4b5
Vladislav Korzun <vladkorzun2016@yandex.ru>
2025-05-07 22:21:39 +0300

    add_mainMenuScreen and Display

---
+++ app/src/main/java/ru/mygames/snaker/ui/screens/menu/MainMenuScreen.kt
@@ -0,0 +1,9 @@
+package ru.mygames.snaker.ui.screens.menu
+
+import androidx.compose.runtime.Composable
+import ru.mygames.snaker.ui.screens.menu.views.MainMenuViewDisplay
+
+@Composable
+fun MainMenuScreen() {
+    MainMenuViewDisplay()
+}
--- app/src/main/java/ru/mygames/snaker/ui/theme/components/JetTextBtn.kt
+++ app/src/main/java/ru/mygames/snaker/ui/theme/components/JetTextBtn.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,6 +25,7 @@ fun JetTextButton(
     onClick: () -> Unit
 ) {
     Box(
+        contentAlignment = Alignment.Center,
         modifier = modifier
             .background(MaterialTheme.colorScheme.primaryContainer, shape)
             .clip(shape)
@@ -32,8 +34,8 @@ fun JetTextButton(
     ) {
         Text(
             text = text,
-            style = MaterialTheme.typography.bodyLarge,
-            color = MaterialTheme.colorScheme.onPrimaryContainer
+            style = MaterialTheme.typography.titleLarge,
+            color = MaterialTheme.colorScheme.onPrimaryContainer,
         )
     }
 }
